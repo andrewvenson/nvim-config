814e7a59f5b4fb96798c06904ba64a3341172f20
Andrew Venson <vensoneandrew@gmail.com>
2024-09-22 08:33:24 -0500

    Removes the diagnostic list I never use

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -30,8 +30,6 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'isort', 'black' },
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
