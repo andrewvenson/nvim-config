e0631df391b22e045085fca29e61b2ba303dc99e
Andrew Venson <vensoneandrew@gmail.com>
2024-09-27 00:01:28 -0500

    betta save

--- lua/sets/init.lua
+++ lua/sets/init.lua
@@ -1,7 +1,6 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 local config = vim.fn.stdpath 'config'
 local macro = vim.fn.readfile(config .. '/lua/macros/interface-to-object')
-print(macro)
 vim.fn.setreg('a', macro)
 
 vim.g.loaded_netrwPlugin = 1
